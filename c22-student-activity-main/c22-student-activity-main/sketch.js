07bd3567f809afcdf1c429e97808ca322f342c35
King802-byte <82222803+King802-byte@users.noreply.github.com>
2021-06-24 19:42:13 +0530

    Add files via upload

---
+++ c22-student-activity-main/c22-student-activity-main/sketch.js
@@ -0,0 +1,37 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var object,ground;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    var object_options={
+        restitution:1.3
+    }
+    object = Bodies.circle(200,200,30,object_options);
+    World.add(world,object);
+
+    var ground_options={
+        isStatic:true
+    }
+    ground = Bodies.rectangle(200,380,400,20,ground_options);
+    World.add(world,ground);
+    
+    console.log(object);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+
+    ellipseMode(RADIUS);
+    ellipse(object.position.x,object.position.y,30,30)
+
+    rectMode(CENTER);
+    rect(ground.position.x,ground.position.y,400,20)
+}
